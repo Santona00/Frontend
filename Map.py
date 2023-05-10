e89f084195b64aeeff0b9f9455cea7ed23c7519a
Chowdhury farjana tur santona <84067666+Santona00@users.noreply.github.com>
2023-05-10 20:36:27 +0600

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,7 +1,7 @@
 import json
 import plotly.colors as colors
 from plotly.offline import iplot, init_notebook_mode
-from matplotlib import cm
+# from matplotlib import cm
 import plotly.io as pio
 import plotly.express as px
 import numpy as np
