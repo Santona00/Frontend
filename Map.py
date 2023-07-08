0a505a1f88c6ece1cee3a85fbe1d8f5e14251fa7
Santona <farzana.tur@gmail.com>
2023-07-09 00:29:22 +0600

    folium map added2

--- Map.py
+++ Map.py
@@ -15,10 +15,6 @@ import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 
 
-
-
-
-
 geojson_data = load(
     open('bangladesh_geojson_adm2_64_districts_zillas.json', 'r'))
 geojson_data['features'][61].keys()
@@ -60,31 +56,38 @@ grouped4 = final_data.groupby(
 month_data = grouped4.copy()
 # month_data
 
-grouped5 = final_data.groupby(['id', 'District', 'year', 'lat', 'lon'])[
+grouped5 = final_data.groupby(['id', 'LOCATION', 'District', 'year', 'lat', 'lon'])[
     'Accidents'].sum().reset_index()
 year_data = grouped5.copy()
 # year_data
 
 # ---------------------------------------------------streamlit-----------------
-row1_col1, row1_col2 = st.columns(
-    [0.8, 2]
+row1_col1, row1_col2, row1_col3 = st.columns(
+    [0.5, 1.5, 1]
 )
-row2_col1, row2_col2 = st.columns(
-    [1.5, 1.5]
+row2_col1, row2_col2, row2_col3 = st.columns(
+    [0.5, 0.5, 2]
 )
-row3_col1, row3_col2, row3_col3 = st.columns(
-    [1, 1, 1]
+row3_col1, row3_col2, row3_col3, row3_col4 = st.columns(
+    [0.5, 0.5, 0.5, 1.5]
 )
-row4_col1, row4_col2, row4_col3 = st.columns(
-    [1, 1, 1]
+row4_col1, row4_col2, row4_col3, row4_col4 = st.columns(
+    [0.5, 0.5, 0.5, 1.5]
 )
 row5_col1, row5_col2 = st.columns(
     [1, 1]
 )
 
-row6_col1, row6_col2 = st.columns(
-    [1.5,1.5]
+row6_col1, row6_col2, row6_col3 = st.columns(
+    [0.9, 0.1, 0.9]
 )
+row7_col1, row7_col2, row7_col3 = st.columns(
+    [0.9, 0.1, 0.9]
+)
+row8_col1, row8_col2, row8_col3 = st.columns(
+    [0.9, 0.1, 0.9]
+)
+
 # --------------------------------------------------------------------------------
 with row1_col1:
     time_period = st.selectbox(
@@ -123,13 +126,13 @@ def day_func(yy, mm, dd):
     return filtered
 
 
-def wday_func(yy, mm, ww, dd):
-    filtered = pd.DataFrame()
-    # time_data
-    for index, row in time_data.iterrows():
-        if ((row['year'] == yy) & (row['month'] == mm) & (row['week'] == ww) & (row['day'] == dd)):
-            filtered = pd.concat([filtered, row.to_frame().T])
-    return filtered
+# def wday_func(yy, mm, ww, dd):
+#     filtered = pd.DataFrame()
+#     # time_data
+#     for index, row in time_data.iterrows():
+#         if ((row['year'] == yy) & (row['month'] == mm) & (row['week'] == ww) & (row['day'] == dd)):
+#             filtered = pd.concat([filtered, row.to_frame().T])
+#     return filtered
 
 
 # ----------------------------------all parts-----------------------------
@@ -155,7 +158,7 @@ if time_period == "Weekly":
         year = st.slider("Select year:", 2020, 2023, 2020)
     with row3_col2:
         month = st.slider("Select month:", 1, 12, 11)
-    with row3_col3:
+    with row3_col2:
         week = st.slider("Select week:", 1, 52, 1)
     y = year
     m = month
@@ -169,22 +172,34 @@ if time_period == "Daily":
         month = st.slider("Select month:", 1, 12, 11)
     with row4_col3:
         day = st.slider("Select day of the month:", 1, 31, 1)
-        with row5_col1:
-            show_day = st.checkbox('Show week:')
+        # with row5_col1:
+        #     show_day = st.checkbox('Show week:')
     y = year
     m = month
     d = day
     filtered_data = day_func(y, m, d)
-    if show_day:
-        week = st.slider("Select week:", 1, 52, 1)
-        w = week
-        filtered_data = wday_func(y, m, w, d)
+    # if show_day:
+    #     week = st.slider("Select week:", 1, 52, 1)
+    #     w = week
+    #     filtered_data = wday_func(y, m, w, d)
 
 # ==================================================================
 
 
+# =========================================================
+
+
 if 'Accidents' in filtered_data:
-    with row6_col1:
+    with row1_col3:
+        total_accidents = filtered_data['Accidents'].sum()
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Total accident", total_accidents, total_accidents)
+        col2.metric("Highest accident location", "9 mph", "-8 %")
+        col3.metric("Humidity", "86%", "4 %")
+        st.markdown('</div>', unsafe_allow_html=True)
+
+    with row7_col1:
+        # st.info("Deaths density by locations:")
         colorscale = [
             [0, "rgb(255, 255, 255)"],
             [0.1, "rgb(255, 235, 235)"],
@@ -208,8 +223,8 @@ if 'Accidents' in filtered_data:
             colorscale=colorscale,
             zmin=filtered_data['Accidents'].min(),
             zmax=filtered_data['Accidents'].max(),
-            marker_opacity=0.7,
-            marker_line_width=0.7,
+            marker_opacity=0.8,
+            marker_line_width=0.6,
             marker_line_color='rgb(0, 0, 0)',
             text=filtered_data['District'],
             hovertemplate='<b>%{text}</b><br>Accidents: %{z}<extra></extra>'
@@ -231,45 +246,54 @@ if 'Accidents' in filtered_data:
         fig.update_layout(
             height=600,
             width=700
-           
+
         )
 
         st.plotly_chart(fig)
-        
-        #=========================================================
-        
-    with row6_col2:
+
+        # =========================================================
+
+    with row6_col1:
+        # st.info("Deaths density by districts:")
+        color_scale = [
+            "#F70D1A", "#F62817", "#E42217", "#E41B17", "#DC381F", "#C24641",
+            "#C11B17", "#B22222", "#B21807", "#A52A2A", "#A70D2A", "#9F000F",
+            "#931314", "#990000", "#990012", "#8B0000", "#800000", "#8C001A",
+            "#7E191B", "#800517"
+        ]
         m = folium.Map(location=[23.6850, 90.3563],
-                        zoom_start=7, width="%100", height="%100")
+                       zoom_start=7, width="%60", height="%100")
         grouped_data = filtered_data.groupby(
-            'District')['Accidents'].sum().reset_index()
+            'LOCATION')['Accidents'].sum().reset_index()
         max_accidents = grouped_data['Accidents'].max()
         for index, row in grouped_data.iterrows():
-            district = row['District']
+            location = row['LOCATION']
             accidents = row['Accidents']
-            district_data = filtered_data[filtered_data['District'] == district]
-            for _, row in district_data.iterrows():
+            LOCATION_data = filtered_data[filtered_data['LOCATION'] == location]
+            for _, row in LOCATION_data.iterrows():
                 lat = row['lat']
-                lon = row['lon']            
-                scaling_factor = accidents / max_accidents               
-                max_radius = 20
-                radius = scaling_factor * max_radius               
+                lon = row['lon']
+                scaling_factor = accidents / max_accidents
+                max_radius = 50
+                radius = scaling_factor * max_radius
+                color_index = int(scaling_factor * (len(color_scale) - 1))
+                color = color_scale[color_index]
                 marker = folium.CircleMarker(
                     location=[lat, lon],
                     radius=radius,
-                    color='red',
+                    color=color,
                     fill=True,
-                    fill_color='red'
-                )      
-                tooltip = f"District: {district}<br>Total Accidents: {accidents}"
+                    fill_color=color,
+                    opacity=0.8
+                )
+                tooltip = f"Location: {location}<br>Total Accidents: {accidents}"
                 marker.add_child(folium.Tooltip(tooltip))
                 marker.add_to(m)
         map_html = m._repr_html_()
         st.components.v1.html(map_html, height=700, width=1000)
-            
-                    
-            
+
         # =====================================================
+    with row8_col1:
         show_table = st.checkbox('Show table of data')
 
         if show_table:
@@ -283,7 +307,8 @@ if 'Accidents' in filtered_data:
             start_row = max(0, min(num_rows - page_size, start_row))
             end_row = start_row + page_size if start_row + \
                 page_size <= num_rows else num_rows
-            table_data = filtered_data.iloc[start_row:end_row].to_html(index=False)
+            table_data = filtered_data.iloc[start_row:end_row].to_html(
+                index=False)
             table_style = '<style>table {margin: 0 auto;}</style>'
             container_style = '<style>.container {display: flex; justify-content: center;}</style>'
 
@@ -308,10 +333,9 @@ if 'Accidents' in filtered_data:
             st.markdown(
                 f'<div class="container" style="text-align:center;">{page_info}</div>', unsafe_allow_html=True)
 
-        else:
-            st.error('No data found !')
-            
-            
-    
+        # else:
+        #     st.error('No data found !')
+
+
 else:
     st.error('No data found !')
